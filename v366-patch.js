ee352d1efc29efdcafb695582ad38525862ee41b
sena777xx-stack <sena777xx@gmail.com>
2026-07-31 09:52:19 +0900

    Prevent attacks against active characters

---
+++ v366-patch.js
@@ -0,0 +1,12 @@
+import{GameEngine}from'./game-engine-v3.js?v=3441';
+
+const baseDeclareAttack=GameEngine.prototype.declareAttack;
+GameEngine.prototype.declareAttack=async function(side,attackerId,targetId){
+  const defender=side==='player'?'ai':'player';
+  const targetCard=this.state?.sides?.[defender]?.field?.find(card=>card.uid===targetId);
+  if(targetCard&&!targetCard.rested){
+    this.log('ルール確認：アクティブのキャラにはアタックできません');
+    return false;
+  }
+  return baseDeclareAttack.call(this,side,attackerId,targetId);
+};
